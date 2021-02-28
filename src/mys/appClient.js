6c143e17956c8287fe206d0acec3c597c2537d2f
Cyb <280169928@qq.com>
2021-02-28 11:32:46 +0800

    米游社APP  添加米游币校验

--- src/mys/appClient.js
+++ src/mys/appClient.js
@@ -7,6 +7,7 @@ const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 module.exports = class MysAppClient {
     constructor(appCookie, missionAppCookie) {
+        this.now_missions = 0;
         this.app_headers = {
             'x-rpc-device_id': v4(),
             'x-rpc-client_type': '2',
@@ -24,11 +25,26 @@ module.exports = class MysAppClient {
         };
     }
 
+    get_user_missions_state() {
+        return (async () => {
+            if (this.mission_headers.cookie != null) {
+                _log('查询当日已获得米游币...');
+                await get('https://api-takumi.mihoyo.com/apihub/wapi/getUserMissionsState?point_sn=myb', {
+                    headers: this.mission_headers,
+                }).then(data => {
+                    this.now_missions = _.get(data, 'data.data.already_received_points');
+                }).catch(e => {
+                    global.failed = true;
+                    _err('米游币查询失败', JSON.stringify(e));
+                })
+            }
+        })()
+    }
 
     continuous_sign() {
         post(
             `https://api-takumi.mihoyo.com/apihub/sapi/signIn`,
-            {"gids": "2"},
+            {'gids': '2'},
             {headers: {...this.app_headers, ds: appds()}}
         ).catch(e => {
             global.failed = true;
@@ -68,51 +84,47 @@ module.exports = class MysAppClient {
     }
 
     appMybCheckin() {
-        (async () => {
-            await get(`https://bbs-api.mihoyo.com/post/api/feeds/posts?fresh_action=1&gids=2&last_id=`, {
-                headers: this.app_headers,
-            }).then(async ({data}) => {
-                const list = _.get(data, 'data.list');
-                let post_id_list = _.map(list, obj => {
-                    return _.get(obj, 'post.post_id');
-                });
-                for (let post_id of post_id_list.slice(0, 1)) {
+        return (async () => {
+            await this.get_user_missions_state();
+            if (this.now_missions !== 110) {
+                _log(`已获得${this.now_missions}米游币,继续执行`);
+                await get(`https://bbs-api.mihoyo.com/post/api/feeds/posts?fresh_action=1&gids=2&last_id=`, {
+                    headers: this.app_headers,
+                }).then(async ({data}) => {
+                    const list = _.get(data, 'data.list');
+                    let post_id_list = _.map(list, obj => {
+                        return _.get(obj, 'post.post_id');
+                    });
+                    for (let post_id of post_id_list.slice(0, 1)) {
+                        await delay(1000);
+                        this.continuous_sign(post_id)
+                    }
+                    for (let post_id of post_id_list.slice(0, 3)) {
+                        await delay(1000);
+                        this.view_post_0(post_id)
+                    }
+                    for (let post_id of post_id_list.slice(0, 5)) {
+                        await delay(1000);
+                        this.post_up_0(post_id)
+                    }
+                    for (let post_id of post_id_list.slice(0, 1)) {
+                        await delay(1000);
+                        this.share_post_0(post_id)
+                    }
                     await delay(1000);
-                    this.continuous_sign(post_id)
-                }
-                for (let post_id of post_id_list.slice(0, 3)) {
-                    await delay(1000);
-                    this.view_post_0(post_id)
-                }
-                for (let post_id of post_id_list.slice(0, 5)) {
-                    await delay(1000);
-                    this.post_up_0(post_id)
-                }
-                for (let post_id of post_id_list.slice(0, 1)) {
-                    await delay(1000);
-                    this.share_post_0(post_id)
-                }
-                _log("米游币获取完成,如有配置missionCookie将请求任务校验接口");
-            }).catch(e => {
-                global.failed = true;
-                _err('米游币获取请求失败', e.toString());
-            });
-            await delay(1000);
-            if (this.mission_headers.cookie != null) {
-                _log("访问任务校验接口...");
-                get('https://api-takumi.mihoyo.com/apihub/wapi/getUserMissionsState?point_sn=myb', {
-                    headers: this.mission_headers,
-                }).then(data => {
-                    if (_.get(data, 'data.data.already_received_points') === 110) {
-                        _log("米游币校验成功");
+                    await this.get_user_missions_state();
+                    if (this.now_missions === 110) {
+                        _log('今日已获得110米游币');
                     } else {
                         global.failed = true;
-                        _err('米游币校验失败', data.toString());
+                        _err('米游币不满足110,请手动检查');
                     }
                 }).catch(e => {
                     global.failed = true;
-                    _err('米游币校验失败', e.toString());
-                })
+                    _err('米游币获取请求失败', e.toString());
+                });
+            } else {
+                _warn(`已获得${this.now_missions}米游币,无需继续执行`);
             }
         })()
 
